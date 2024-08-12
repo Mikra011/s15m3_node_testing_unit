733e8f8a135cdf1f0ddb170b4379159178ac0c4d
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-08-12 10:38:55 +0200

    started

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,28 @@
+function foo () {
+    return 'foo'
+}
+
 // Test away!
+describe('our first tests', () => {
+    test('sanity', () => {
+        expect(5).toBe(5)
+        expect(5).toBeGreaterThan(1)
+    })
+    test('objects', () => {
+        const o = { a: 1 }
+        const oo = { a: 1 }
+        const ooo = oo
+        expect(o).toBe(o)
+        expect(oo).toBe(ooo)
+    })
+    test('objects again', () => {
+        expect({ a: 1 }).toEqual({ a: 1 })
+    })
+})
+
+describe('foo function', () => {
+    test('foo returns foo', () => {
+        expect(foo()).toBe('foo')
+        expect(foo()).toHaveLength(3)
+    })
+})
